afdc7a495cf50f27402dabef2a643a916a9e2abe
Viacheslav <dvo@tuta.io>
2022-06-21 09:14:54 +0300

    Create models skeleton

--- app/config/config.php
+++ app/config/config.php
@@ -11,9 +11,9 @@ return new \Phalcon\Config([
     'database' => [
         'adapter'     => 'Mysql',
         'host'        => 'localhost',
-        'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
+        'username'    => 'phalcon',
+        'password'    => '1234',
+        'dbname'      => 'url_checker',
         'charset'     => 'utf8',
     ],
     'application' => [
