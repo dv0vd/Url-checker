f99fdbd6af6cc27b8769f4ddb1955aef7d8b6003
Viacheslav <dvo@tuta.io>
2022-06-23 22:19:36 +0300

    Fixed types conversion

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $("#addUrlForm").submit(function(e){
     e.preventDefault();
-    if($("#repeats").val() >= $("#freq").val()) {
+    if(parseInt($("#repeats").val()) >= parseInt($("#freq").val())) {
       alert("Количество повторов должно быть меньше частоты проверки!");
       return;
     }
